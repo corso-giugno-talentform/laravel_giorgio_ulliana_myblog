52803be9db80492378df3b488c23fa02c04cb92d
GiorgioUll <giorgio.ulliana@gmail.com>
2025-07-28 10:44:11 +0200

    primo blog

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [PageController::class, 'homepage'])->name('homepage');
+Route::get('/blog/{dettaglio}', [PageController::class, 'blogdettaglio'])->name('dblogdettaglio');
+
+
+
+
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
